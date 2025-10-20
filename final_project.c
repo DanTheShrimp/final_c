7df1861c12398188b43e743cabee3a4ce2338666
DanTheShrimp <daniel.delong@ucas-edu.net>
2025-10-20 14:28:32 -0600

    started :D

---
+++ final_project.c
@@ -0,0 +1,39 @@
+// DD LC RV JD 7th Final Project For CP
+
+#include <stdio.h>
+
+char board(void){
+
+    printf("    |    |    \n");
+    printf("    |    |    \n");
+    printf("----|----|----\n");
+    printf("    |    |    \n");
+    printf("    |    |    \n");
+    printf("----|----|----\n");
+    printf("    |    |    \n");
+    printf("    |    |    \n");
+}
+//Fernando
+char x(void){
+    printf("")
+}
+
+
+
+// Lindon, fuctions
+
+// Daniel, print fuctions
+
+// Jesucristo, If staments
+, print questions
+// Lindon,
+
+// Daniel, 
+staments
+
+int main(void){
+    
+    printf("Would you like to be X or O");
+    scanf("")
+    printf("Where would you like to place your ")
+}
