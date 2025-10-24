de3ec2f1c1dc3a29ba3c9f09ef9c4cbf7068ccc7
DanTheShrimp <daniel.delong@ucas-edu.net>
2025-10-24 16:19:38 -0600

    FINISHED

--- final_project.c
+++ final_project.c
@@ -6,7 +6,9 @@
 #include <stdlib.h>
 #include <windows.h>
 
-
+char playername[2];
+int player = 0;
+int computer = 0;
 char board[3][3]={{' ',' ',' '},
                 {' ',' ',' '},
                 {' ',' ',' '}};
@@ -19,14 +21,90 @@ char board[3][3]={{' ',' ',' '},
 
 // Jesus, win conditions, if staments, check speling
 
+void name(void){ // daniel and lindon
+    printf("Hello, what are your initials (Two letters): \n");
+    scanf("%s", &playername);
+}
 
-int computerturn(void){
+int checker(void){ // dan lindon and romeo
+    //player check
+    if(board[0][0] == 'X' && board[0][1] == 'X' && board[0][2] == 'X'){
+        player = 1;
+    }else if(board[1][0] == 'X' && board[1][1] == 'X' && board[1][2] == 'X'){
+        player = 1;
+    }else if(board[2][0] == 'X' && board[2][1] == 'X' && board[2][2] == 'X'){
+        player = 1;
+    }else if(board[0][0] == 'X' && board[1][0] == 'X' && board[2][0] == 'X'){
+        player = 1;
+    }else if(board[0][1] == 'X' && board[1][1] == 'X' && board[2][1] == 'X'){
+        player = 1;
+    }else if(board[0][2] == 'X' && board[1][2] == 'X' && board[2][2] == 'X'){
+        player = 1;
+    }else if(board[0][0] == 'X' && board[1][1] == 'X' && board[2][2] == 'X'){
+        player = 1;
+    }else if(board[0][2] == 'X' && board[1][1] == 'X' && board[2][0] == 'X'){
+        player = 1;
+    }
+    if(board[0][0] == 'O' && board[0][1] == 'O' && board[0][2] == 'O'){
+        computer = 1;
+    }else if(board[1][0] == 'O' && board[1][1] == 'O' && board[1][2] == 'O'){
+        computer = 1;
+    }else if(board[2][0] == 'O' && board[2][1] == 'O' && board[2][2] == 'O'){
+        computer = 1;
+    }else if(board[0][0] == 'O' && board[1][0] == 'O' && board[2][0] == 'O'){
+        computer = 1;
+    }else if(board[0][1] == 'O' && board[1][1] == 'O' && board[2][1] == 'O'){
+        computer = 1;
+    }else if(board[0][2] == 'O' && board[1][2] == 'O' && board[2][2] == 'O'){
+        computer = 1;
+    }else if(board[0][0] == 'O' && board[1][1] == 'O' && board[2][2] == 'O'){
+        computer = 1;
+    }else if(board[0][2] == 'O' && board[1][1] == 'O' && board[2][0] == 'O'){
+        computer = 1;
+    }
+    if (player == 1){ // dan and lindon
+        printf("|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|\n");
+        printf("|                                   |\n");
+        printf("|                                   |\n");
+        printf("|                                   |\n");
+        printf("|                                   |\n");
+        printf("|                                   |\n");
+        printf("|              %s wins!             |\n", playername);
+        printf("|                                   |\n");
+        printf("|                                   |\n");
+        printf("|                                   |\n");
+        printf("|                                   |\n");
+        printf("|                                   |\n");
+        printf("|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|\n");
+        exit;
+
+    }else if (computer == 1){
+        printf("|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|\n");
+        printf("|                                   |\n");
+        printf("|                                   |\n");
+        printf("|                                   |\n");
+        printf("|                                   |\n");
+        printf("|                                   |\n");
+        printf("|           Computer wins!          |\n");
+        printf("|                                   |\n");
+        printf("|                                   |\n");
+        printf("|                                   |\n");
+        printf("|                                   |\n");
+        printf("|                                   |\n");
+        printf("|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|\n");
+        exit;
+
+    }
+    return 0;
+}
+
+int computerturn(void){ // dan
     srand(time(NULL));
-    int compturn1 = rand() % 3;
-    int compturn2 = rand() % 3;
     printf("The computer will go now. Please wait \n");
     Sleep(1000);
     while (TRUE){
+        int compturn1 = rand() % 3;
+        int compturn2 = rand() % 3;
         if (board[compturn1][compturn2] == ' '){
             board[compturn1][compturn2] = 'O';
             printf(" %c|%c|%c\n", board[0][0], board[0][1], board[0][2]);
@@ -36,16 +114,12 @@ int computerturn(void){
             printf(" %c|%c|%c\n", board[2][0], board[2][1], board[2][2]);
             Sleep(2000);
             break;
-        }else{
-            int compturn1 = rand() % 3;
-            int compturn2 = rand() % 3;
-            continue;
         }
     }
     return 0;
 }
 
-int playerturn(void){
+int playerturn(void){ //dan and romeo
     int move1;
     int move2;
     while (TRUE){
@@ -65,73 +139,32 @@ int playerturn(void){
         }
         
     }
+    
     return 0;
 }
 
-
-int main(void){
-    char playername[2];
-    int player = 0;
-    int computer = 0;
+int main(void){ // everyone
     printf("-----Tick Tack Toe-----\n");
-    printf("Hello, what are your initials (Two letters): \n");
-    scanf("%s", &playername);
+    name();
     playerturn();
+    checker();
     computerturn();
+    checker();
     playerturn();
+    checker();
     computerturn();
+    checker();
     playerturn();
-    if(board[0][0] == 'X' && board[0][1] == 'X' && board[0][2] == 'X'){
-        player = 1;
-    }else if(board[1][0] == 'X' && board[1][1] == 'X' && board[1][2] == 'X'){
-        player = 1;
-    }else if(board[2][0] == 'X' && board[2][1] == 'X' && board[2][2] == 'X'){
-        player = 1;
-    }else if(board[0][0] == 'X' && board[1][0] == 'X' && board[2][0] == 'X'){
-        player = 1;
-    }else if(board[0][1] == 'X' && board[1][1] == 'X' && board[2][1] == 'X'){
-        player = 1;
-    }else if(board[0][2] == 'X' && board[1][2] == 'X' && board[2][2] == 'X'){
-        player = 1;
-    }else if(board[0][0] == 'X' && board[1][1] == 'X' && board[2][2] == 'X'){
-        player = 1;
-    }else if(board[0][2] == 'X' && board[1][1] == 'X' && board[2][0] == 'X'){
-        player = 1;
-    }
-    
-    
-
-    if (player == 1){
-        printf("|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|\n");
-        printf("|                                   |\n");
-        printf("|                                   |\n");
-        printf("|                                   |\n");
-        printf("|                                   |\n");
-        printf("|                                   |\n");
-        printf("|              %s wins!             |\n", playername);
-        printf("|                                   |\n");
-        printf("|                                   |\n");
-        printf("|                                   |\n");
-        printf("|                                   |\n");
-        printf("|                                   |\n");
-        printf("|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|\n");
-
-    }else if (computer == 1){
-        printf("|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|\n");
-        printf("|                                   |\n");
-        printf("|                                   |\n");
-        printf("|                                   |\n");
-        printf("|                                   |\n");
-        printf("|                                   |\n");
-        printf("|           Computer wins!          |\n");
-        printf("|                                   |\n");
-        printf("|                                   |\n");
-        printf("|                                   |\n");
-        printf("|                                   |\n");
-        printf("|                                   |\n");
-        printf("|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|\n");
-
-    } else{
+    checker();
+    computerturn();
+    checker();
+    playerturn();
+    checker();
+    computerturn();
+    checker();
+    playerturn();
+    checker();
+    if (player == 0 && computer == 0){
         printf("|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|\n");
         printf("|                                   |\n");
         printf("|                                   |\n");
